e6d3cc925eb195bbd07886e63a4e8ade4e1d461a
Uwe Gruenefeld <info@uwe-gruenefeld.de>
2017-10-07 10:37:24 -0400

    Updated server and unity * Build in options to debug, disable and fake data * Added Reciever on Unity-Site

--- server/server.js
+++ server/server.js
@@ -8,48 +8,73 @@
  * @version 2017-10-07
  **/
 
- var config		= require('./config.json');
+var config = require('./config.json');
 
- /*
-  * Connection to Unity
-  */
- var Websocket   = require("nodejs-websocket");
- var unity	     = Websocket.createServer(function(connection)
- {
- 	console.log("New socket connection");
-
-  // Handle event for incoming text
-  connection.on("text", function(message)
+/*
+ * Connection to Unity
+ */
+if(config.unity.active)
+{
+  var Websocket   = require("nodejs-websocket");
+  var unity	      = Websocket.createServer(function(connection)
   {
+    if(config.debug)
+      console.log("New socket connection");
 
+    // Fake it till you make it
+    if(config.unity.fake)
+    {
+      function rand() {
+        return Math.floor(Math.random() * config.arduino.servo);
+      }
+
+      function fake() {
+        connection.sendText(rand() + ' ' + rand() + ' ' + rand());
+        setTimeout(fake, 1000);
+      }
+      fake();
+    }
+
+    // Handle event for incoming text
+    connection.on("text", function(message)
+    {
+
+    });
+
+    // Handle event for close connection
+  	connection.on("close", function()
+  	{
+      if(config.debug)
+  		  console.log("Socket connection closed");
+  	});
   });
-
-  // Handle event for close connection
-	connection.on("close", function()
-	{
-		console.log("Socket connection closed");
-	});
-});
-unity.listen(config.port);
-console.log('Unity connection is running on ws://localhost:' + config.unityPort);
+  unity.listen(config.unity.port);
+  if(config.debug)
+    console.log('Unity connection is running on ws://localhost:' + config.unity.port);
+}
 
 /*
  * Connection to Arduino
  */
-var SerialPort = require("serialport");
-var arduino = new SerialPort("/dev/tty.usbmodem" + config.arduinoPort, {
-  baudrate: 9600,
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  flowControl: false
-});
-
-arduino.on('open', function()
+if(config.arduino.active)
 {
-  arduino.on('data', function(data)
-	{
-      console.log(data[0]);
+  var SerialPort = require("serialport");
+  var arduino = new SerialPort("/dev/tty.usbmodem" + config.arduinoPort, {
+    baudrate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
   });
-});
-console.log('Arduino connection is running on /dev/tty.usbmodem' + config.arduinoPort);
+
+  arduino.on('open', function()
+  {
+    arduino.on('data', function(data)
+  	{
+      if(config.debug)
+        console.log(data[0]);
+    });
+  });
+  if(config.debug)
+    console.log('Arduino connection is running on /dev/tty.usbmodem' + config.arduino.port);
+}
---
+++ unity/Assets/Scripts/Arduino.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * VR-OR Connection to Arduino
+ *
+ * @file Arduino.cs
+ * @author Uwe Gruenefeld
+ * @version 2017-10-07
+ **/
+﻿using System;
+using WebSocketSharp;
+
+namespace UnityEngine
+{
+	/// <summary>
+	/// Implements MonoBehaviour and connects to server via websocket
+	/// </summary>
+	public class Arduino : MonoBehaviour
+	{
+		[HeaderAttribute("Connection")]
+		public string IP = "localhost";
+		public int PORT = 8080;
+
+		// Variables for websocket connection
+		private WebSocket webSocket;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		/// <summary>
+		/// Update is called once per frame by Unity
+		/// </summary>
+		void Update () 
+		{
+			// Check connection
+			if (!this.ProcessConnection ())
+				return;
+		}
+
+		/// <summary>
+		/// Checks the websocket connection
+		/// </summary>
+		/// <returns><c>true</c>, if connection was established, <c>false</c> otherwise.</returns>
+		private bool ProcessConnection()
+		{
+			// If the websocket is null
+			if(this.webSocket == null)
+				this.webSocket = new WebSocket ("ws://" + this.IP + ":" + this.PORT);
+
+			// If the websocket is not alive
+			// try to connect to the server
+			if (!this.webSocket.IsAlive)
+			{
+				try
+				{
+					// Connect to the server
+					this.webSocket.Connect ();
+
+					// Method for handling incoming messages
+					this.webSocket.OnMessage += (sender, message) => this.Response (message.Data);
+				}
+				catch(Exception exception) 
+				{
+					// Something went wrong
+					Debug.Log (exception);
+				}
+			}
+
+			// Return if websocket is alive
+			return this.webSocket.IsAlive;
+		}
+
+		// Handling a response
+		private void Response(string message)
+		{
+			Debug.Log ("Recieved: " + message);
+		}
+	}
+}
