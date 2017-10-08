4c89bf4b77621c38ddce288c664dd7774e6822a8
Uwe Gruenefeld <info@uwe-gruenefeld.de>
2017-10-08 15:59:26 -0400

    First version ready * Added two first builds * Unity ready

---
+++ unity/Assets/Scripts/Controller/RoboticController.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Mo Kakwan, Uwe Gruenefeld, Helena Deus, Michal Leszczynski, Alisha Haris
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MagicTool
+{
+	public class RoboticController : MonoBehaviour 
+	{
+		[Header("Arm Controller")]
+		public float angle1;
+		public float angle2;
+		public float angle3;
+
+		// GameObjects for parts
+		private GameObject armA, armB;
+		private GameObject armA_anchor, armB_anchor;
+
+		/// <summary>
+		/// Start is called once at startup
+		/// </summary>
+		void Start () 
+		{
+			this.armA = GameObject.Find ("ArmA");
+			this.armB = GameObject.Find ("ArmB");
+
+			this.armA_anchor = GameObject.Find ("ArmA_Anchor");
+			this.armB_anchor = GameObject.Find ("ArmB_Anchor");
+		}
+
+		/// <summary>
+		/// Update is called once per frame by Unity
+		/// </summary>
+		void Update () 
+		{
+			this.armA_anchor.transform.rotation = Quaternion.Euler (angle2, angle1, 0);
+			this.armB_anchor.transform.rotation = Quaternion.Euler (angle3, 0, 0);
+		}
+	}
+}
---
+++ unity/Assets/Scripts/MeshFilter/Deformer.cs
@@ -0,0 +1,77 @@
+﻿/**
+ * IMPORTANT
+ * 
+ * BASED ON SOURCECODE FROM 
+ * Jasper Flick
+ * http://catlikecoding.com/unity/tutorials/mesh-deformation/
+ * 
+ * We modified the code to fit our needs.
+ * 
+ **/
+using UnityEngine;
+
+namespace MagicTool 
+{
+	[RequireComponent(typeof(MeshFilter))]
+	public class Deformer : MonoBehaviour 
+	{
+		public float springForce = 20f;
+		public float damping = 5f;
+
+		private Mesh deformingMesh;
+		private Vector3[] originalVertices, displacedVertices, vertexVelocities;
+		float uniformScale = 1f;
+
+		private void Start () 
+		{
+			deformingMesh = GetComponent<MeshFilter>().mesh;
+			originalVertices = deformingMesh.vertices;
+			displacedVertices = new Vector3[originalVertices.Length];
+			for (int i = 0; i < originalVertices.Length; i++) {
+				displacedVertices[i] = originalVertices[i];
+			}
+			vertexVelocities = new Vector3[originalVertices.Length];
+		}
+
+		private void Update () 
+		{
+			uniformScale = transform.localScale.x;
+			for (int i = 0; i < displacedVertices.Length; i++) {
+				UpdateVertex(i);
+			}
+			deformingMesh.vertices = displacedVertices;
+			deformingMesh.RecalculateNormals();
+		}
+
+		private void UpdateVertex (int i) 
+		{
+			Vector3 velocity = vertexVelocities[i];
+			Vector3 displacement = displacedVertices[i] - originalVertices[i];
+			displacement *= uniformScale;
+			velocity -= displacement * springForce * Time.deltaTime;
+			velocity *= 1f - damping * Time.deltaTime;
+			vertexVelocities[i] = velocity;
+			displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+		}
+
+		public void AddDeformingForce (Vector3 point, float force) 
+		{
+			point = transform.InverseTransformPoint(point);
+
+			for (int i = 0; i < displacedVertices.Length; i++) 
+			{
+				AddForceToVertex(i, point, force);
+			}
+		}
+
+		private void AddForceToVertex (int i, Vector3 point, float force) 
+		{
+			Vector3 pointToVertex = displacedVertices[i] - point;
+			pointToVertex *= uniformScale;
+
+			float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+			float velocity = attenuatedForce * Time.deltaTime;
+			vertexVelocities[i] += pointToVertex.normalized * velocity;
+		}
+	}
+}
